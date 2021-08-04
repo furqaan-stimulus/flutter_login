636e023fc45bec390c3f4ce1524c6ea53fc2778b
Furqaan mirza <furqaan.stimulus@gmail.com>
2021-08-04 12:57:36 +0530

    Login using bloc

---
+++ lib/app.dart
@@ -0,0 +1,78 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/authentication/bloc/authentication_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+import 'home/view/home_page.dart';
+import 'login/view/login_page.dart';
+import 'splash/view/splash_page.dart';
+
+class App extends StatelessWidget {
+  final AuthenticationRepository authenticationRepository;
+  final UserRepository userRepository;
+
+  const App({
+    Key? key,
+    required this.authenticationRepository,
+    required this.userRepository,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+          userRepository: userRepository,
+        ),
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState!;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                _navigator.pushAndRemoveUntil(
+                  HomePage.route(),
+                  (route) => false,
+                );
+                break;
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+                break;
+              default:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/login/login.dart
@@ -0,0 +1,3 @@
+export 'view/view.dart';
+export 'view/login_form.dart';
+export 'view/login_page.dart';
